6761face3cd05b756d6195be2b0e44191212c9fb
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-02 13:41:38 +0200

    clase acabada de verdad

--- js/Escudero.js
+++ js/Escudero.js
@@ -1,10 +1,20 @@
-class Escudero {
-  senyor;
+import { Personaje } from "./Personaje";
 
-  constructor(nombre, familia, edad, serie, senyor) {
+class Escudero extends Personaje {
+  senyor;
+  pelotismo;
+
+  constructor(nombre, familia, edad, serie, senyor, pelotismo) {
     super(nombre, familia, edad, serie);
     this.senyor = senyor;
+    if (pelotismo < 1) {
+      this.pelotismo = 1;
+    } else if (pelotismo > 10) {
+      this.pelotismo = 10;
+    } else {
+      this.pelotismo = pelotismo;
+    }
   }
 
-  comunica = () => "No sé por qué, pero creo que voy a morir pronto";
+  comunica = () => "Soy un loser";
 }
